d62cff55c0863726669afe4d4d7618fa5e24b0a0
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-01-09 14:03:57 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
     counter += 1
     basic.showNumber(counter)
 })
+let strip: neopixel.Strip = null
 let counter = 0
 counter = 0
+strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
