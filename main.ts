c571de490a82a5aee8b6bc941164f31b50d4037f
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-01-09 12:00:41 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    counter += 1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
+basic.forever(function () {
+	
+})
